05d1976ae4f281d5341853dd57326f71458f2dab
isabellexvr <isabellexavier59@gmail.com>
2023-05-07 22:46:27 -0300

    style: ranking two left podium

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,5 +1,6 @@
 export const colors = {
     darkBlue: "#004B5F",
+    mediumBlue: "#00BFC7",
     lightBlue: "#01BEC6",
     lightGrey: "#E5E5E5"
 }
--- src/pages/RankingPage.jsx
+++ src/pages/RankingPage.jsx
@@ -6,6 +6,7 @@ import useGetRanking from "../services/hooks/api/useGetRanking";
 import { useEffect, useState } from "react";
 import { RiTrophyFill } from "react-icons/ri";
 import handlePodiumIcon from "./helpers/handlePodiumIcon.js";
+import { HiCursorClick } from "react-icons/hi";
 
 const MOCKEDPODIUM = [
   {
@@ -86,10 +87,28 @@ export default function RankingPage() {
                     <h1>{p.name}</h1>
                   </div>
                   <h2>{p.linksCount} URLs</h2>
-                  <ViewsCount>{p.visitCount} Views</ViewsCount>
+                  <ViewsCount>
+                    <HiCursorClick />
+                    {p.visitCount} Views
+                  </ViewsCount>
                 </Podium>
               ))}
             </PodiumContainer>
+            <TwoLeftContainer>
+              {MOCKEDLEFT.map((l, i) => (
+                <RankingLeft place={i} key={i}>
+                  <div className="place">
+                    <h1>{i == 0 ? "4th" : "5th"}</h1>
+                  </div>
+                  <h2>{l.name}</h2>
+                  <h3>{l.linksCount} URLs</h3>
+                  <ViewsCount className="views">
+                    <HiCursorClick />
+                    {l.visitCount} Views
+                  </ViewsCount>
+                </RankingLeft>
+              ))}
+            </TwoLeftContainer>
           </RankingContainer>
         </>
       )}
@@ -98,7 +117,10 @@ export default function RankingPage() {
 }
 
 const RankingContainer = styled.div`
-  width: 65vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 55vw;
   height: fit-content;
   background-color: ${colors.darkBlue};
   border-radius: 20px;
@@ -120,8 +142,16 @@ const RankingHeader = styled.div`
     font-size: 43px;
     font-weight: 700;
     height: fit-content;
-    margin-bottom: 15px;
-    color: ${colors.lightBlue};
+    margin-bottom: 10px;
+    color: ${colors.darkBlue};
+    > h1 {
+      cursor: pointer;
+      -webkit-text-stroke: 2px ${colors.lightBlue};
+      :hover {
+        color: yellow;
+        transition: all 0.7s ease;
+      }
+    }
     > svg {
       font-size: 50px;
       margin-right: 10px;
@@ -132,6 +162,7 @@ const RankingHeader = styled.div`
     h2 {
       font-size: 17px;
       font-weight: 500;
+      color: ${colors.lightGrey};
     }
   }
 `;
@@ -144,22 +175,24 @@ const PodiumContainer = styled.div`
 `;
 
 const Podium = styled.div`
-  font-size: 0.9em;
-  width: 30%;
-  padding: 10px;
+  width: 28%;
+  padding: 1.25em;
   box-sizing: border-box;
   border-radius: 15px;
-  height: 15vw;
-  align-items: center;
   background-color: ${colors.lightBlue};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
   .header {
+    font-size: 1.25rem;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     font-weight: 700;
     text-align: center;
     > img {
       width: 2.3em;
-      margin-right: 5px;
     }
   }
   > h2 {
@@ -169,8 +202,9 @@ const Podium = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
-    margin-top: 5px;
-    margin-bottom: 5px;
+    margin-top: 0.8em;
+    margin-bottom: 0.8em;
+
     filter: drop-shadow(0px 0px 1px ${colors.lightGrey});
   }
 `;
@@ -186,4 +220,51 @@ const ViewsCount = styled.div`
   justify-content: center;
   align-items: center;
   font-weight: 700;
+  position: relative;
+  margin: 0 auto;
+  > svg {
+    position: absolute;
+    left: -8px;
+    transform: scaleX(-1);
+    bottom: -8px;
+    font-size: 18px;
+  }
+`;
+
+const TwoLeftContainer = styled.div`
+  margin-top: 15px;
+  width: 75%;
+  height: fit-content;
+`;
+
+const RankingLeft = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  margin-top: 10px;
+  background-color: ${colors.mediumBlue};
+  border-radius: 10px;
+  padding: 9px;
+  font-weight: 500;
+  .place {
+    background-color: ${(p) => (p.place == 0 ? "orange" : "red")};
+    padding: 5px;
+    border-radius: 7px;
+    margin-right: 10px;
+  }
+  .views {
+    font-size: 13px;
+    margin: initial;
+  }
+  > h2 {
+    font-weight: 700;
+  }
+  >h3{
+    color: ${colors.lightGrey};
+    font-size: 13px;
+    font-weight: 700;
+
+    filter: drop-shadow(0px 0px 1px ${colors.lightGrey});
+  }
 `;
