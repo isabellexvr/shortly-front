05d55a5451a30da21789b73a44332b9afba84aab
isabellexvr <isabellexavier59@gmail.com>
2023-05-19 22:06:43 -0300

    feat: login function

--- src/pages/SignInPage/index.jsx
+++ src/pages/SignInPage/index.jsx
@@ -1,21 +1,41 @@
 import Input from "../../components/Input";
-import { FaUsers } from "react-icons/fa";
+import { FaUserCheck } from "react-icons/fa";
 import { Form, Title, StyledLink, Button } from "../styles.js";
+import { useState } from "react";
+import useSignInUser from "../../services/hooks/api/useSignInUser";
 
 export default function SignInPage() {
+  const [form, setForm] = useState({});
+  const { signInUserLoading, signInUserError, signInUser } = useSignInUser();
+
+  const handleForm = ({ target: { value, name } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  const sendForm = async (e) => {
+    e.preventDefault();
+    try {
+      const userInfo = await signInUser(form);
+      console.log(userInfo);
+    } catch (err) {
+      console.log(err);
+      alert(err.response.data);
+    }
+  };
+
   return (
     <>
       <Title>
         <h1>Login</h1>
-        <FaUsers />
+        <FaUserCheck />
       </Title>
-      <Form>
+      <Form onSubmit={sendForm} >
         <Input
           type={"email"}
           id={"email"}
           htmlFor={"email"}
           name={"email"}
-          handleEvent={"handleForm"}
+          handleEvent={handleForm}
         >
           E-mail
         </Input>
@@ -24,17 +44,14 @@ export default function SignInPage() {
           id={"password"}
           htmlFor={"password"}
           name={"password"}
-          handleEvent={"handleForm"}
+          handleEvent={handleForm}
         >
           Senha
         </Input>
         <StyledLink to={"/sign-up"}>
           Ainda não tem uma conta? Cadastre-se!
         </StyledLink>
-        <Button
-          type="submit"
-          fontColor="white"
-        >
+        <Button type="submit" fontColor="white">
           Confirmar
         </Button>
       </Form>
